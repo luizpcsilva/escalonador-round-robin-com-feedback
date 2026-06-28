0b5604b25fc52981c5c608df9e19d1891722a72a
Luiz Paulo <luizp.silva.2005@gmail.com>
2026-06-28 15:40:30 -0300

    inserindo mudanças de processo

---
+++ processo/processo.h
@@ -0,0 +1,36 @@
+#ifndef PROCESSO_H
+#define PROCESSO_H
+
+typedef enum{
+    NOVO,
+    PRONTO,
+    BLOQUEADO,
+    EXECUCAO,
+    TERMINOU
+}statusProcesso;
+
+typedef enum{
+    BAIXA,
+    MEDIA,
+    ALTA
+}prioridadeProcesso;
+
+typedef enum{
+    DISCO,
+    FITA_MAGNETICA,
+    IMPRESSORA
+}tipoIOProcesso;
+
+typedef struct{
+    int pid;
+    int ppid; //indicação do process id do processo pai
+    statusProcesso status;
+    prioridadeProcesso prioridade;
+    int momentoIO; //momento em que o processo vai parar para fazer IO
+    int tempoIO; //tempo que o processo precisa para fazer IO
+    tipoIOProcesso tipoIO;
+} Processo;
+
+
+
+#endif 
