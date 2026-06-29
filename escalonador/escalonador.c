070678fa8735b430a077c6fd07582ed41d48a164
Luiz Paulo <luizp.silva.2005@gmail.com>
2026-06-29 09:04:56 -0300

    implementa parcialmente novas funcionalidades do escalonador

--- escalonador/escalonador.c
+++ escalonador/escalonador.c
@@ -19,7 +19,27 @@ void bootEscalonador(){
 }
 
 void aplicaPreempsao(){
-    
+    if(processoEmExecucao->prioridade < QTD_FILAS){
+        processoEmExecucao->prioridade += 1;
+    } 
+    processoEmExecucao->cpuTimeRestante = QUANTUM;
+
+    //adiciona ele no fim da nova fila de prioridade
+    enfileirarProcesso(processoEmExecucao, processoEmExecucao->prioridade);
+    processoEmExecucao->status= PRONTO;
+
+    //TODO: Admitir novo processo na CPU
+}
+
+void bloquearProcesso(){
+    processoEmExecucao -> status = BLOQUEADO;
+    //TODO: Enviar para fila de IO correspondente
+    //TODO: Admitir novo processo na CPU
+}
+
+void finalizarProcesso(){
+    processoEmExecucao->status = TERMINOU;
+    //TODO: Admitir novo processo na CPU
 }
 
 //TODO boostPrioridade
--- simulador/simulador.c
+++ simulador/simulador.c
@@ -1,5 +1,8 @@
 //TODO Função que soma 1 no tempo do processo em execução 
 //(possivel obte-lo na função getProcessoEmExecucao do escalonador)
 //e verifica se naquele instante o processo pede I/O ou termina. Se
-//sim, mudar estado do processo e chamar aplicaPreempsao do escalonador
+//sim, mudar estado do processo e inserir processo na fila do escalonador (ainda n fiz essa funcao)
 //Essa função vai ser chamada a cada iteração do loop principal
+
+//A cada iteraçao do loop principal, tambem deve verificar se um IO da fila de
+//IO acabou. Nesse caso, deve enviar o processo p fila do escalonador usando a mesma funcao acima
