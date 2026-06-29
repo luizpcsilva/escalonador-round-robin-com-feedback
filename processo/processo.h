dca8aecc11c7acc354f2338a8747c72b7ff9df6e
Maykon de Souza Nascimento <maykon.salles7@gmail.com>
2026-06-29 18:47:51 -0300

    definição de funções
    definição das funções relacionadas à criação do processo, de calculo do tempo e tudo relacionado a tempo, bem como uma função para calcular pid utilizando variavel estatica.
    
    falta as outras funções de destruir, imprimir e mudar o status do processo.

--- processo/processo.h
+++ processo/processo.h
@@ -1,5 +1,6 @@
 #ifndef PROCESSO_H
 #define PROCESSO_H
+#define QUANTUM 10
 
 typedef enum{
     NOVO,
@@ -11,14 +12,14 @@ typedef enum{
 
 typedef enum{
     BAIXA,
-    MEDIA,
     ALTA
 }prioridadeProcesso;
 
 typedef enum{
     DISCO,
     FITA_MAGNETICA,
-    IMPRESSORA
+    IMPRESSORA,
+    SEM_IO
 }tipoIOProcesso;
 
 typedef struct{
@@ -26,11 +27,27 @@ typedef struct{
     int ppid; //indicação do process id do processo pai
     statusProcesso status;
     prioridadeProcesso prioridade;
+    int tempoTotal; //somatorio do tempo que o processo precisa
+    int tempoDecorrido; //somatorio do tempo de execução do processo
     int momentoIO; //momento em que o processo vai parar para fazer IO
     int tempoIO; //tempo que o processo precisa para fazer IO
     tipoIOProcesso tipoIO;
 } Processo;
 
+Processo *criarProcesso(int pid, int ppid);
 
+void destruirProcesso(Processo *processo);
+
+void imprimirProcesso(Processo *processo);
+
+void mudarStatusProcesso(Processo *processo, statusProcesso novoStatus);
+
+int calcTempoTotal(int quantum);
+
+int calcTempoIO (int tempoTotalProcesso);
+
+int calcMomentoIO (int tempoTotalProcesso);
+
+int criaPid ();
 
 #endif 
